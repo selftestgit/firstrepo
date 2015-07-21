8d7862226e5ab7cf8240b62ac3e15b0693a072bc
hemaananth <hema.rananth@gmail.com>
2015-07-21 16:06:36 +1000

    Create Sample.py
    Python script

---
+++ Sample.py
@@ -0,0 +1,10 @@
+#My first python program
+print "Hello, Welcome to Python programming"
+#variables
+name = "Jackson"
+print "welcome" + name
+#arithmetic operators
+a = 10
+b = 15
+c = a+b
+print c
