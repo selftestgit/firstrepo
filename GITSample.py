ca20712c48d10361435fd362dc2220a41f09d7a0
hemaananth <hema.rananth@gmail.com>
2015-07-21 16:23:14 +1000

    Sample Python script

---
+++ GITSample.py
@@ -0,0 +1,14 @@
+#My first python program
+print "Hello, Welcome to Python programming"
+#variables
+name = "Python"
+print "welcome " + name
+#arithmetic operators
+a = 10
+b = 15
+c = a+b
+d = c - 2
+print c
+print d
+if d > 10:
+    print d
